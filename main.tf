51dab3578a27930f086077df50e973fab4d02115
Thelango99 <sanderrf@stud.ntnu.no>
2021-11-18 12:01:22 +0100

    Removed the other VMs. Focusing on a single instance of Linux VM.

--- main.tf
+++ main.tf
@@ -133,83 +133,9 @@ output "tls_private_key" {
     sensitive = true
 }
 
-# Create a virtual machine
-resource "azurerm_linux_virtual_machine" "myterraformvm" {
-    name                  = "myVM"
-    location              = "eastus"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-    size                  = "Standard_DS1_v2"
 
-    os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
 
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
-        version   = "latest"
-    }
 
-    computer_name  = "myvm"
-    admin_username = "azureuser"
-    disable_password_authentication = true
-
-    admin_ssh_key {
-        username       = "azureuser"
-        public_key     = tls_private_key.example_ssh.public_key_openssh
-    }
-
-    boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
-
-    tags = {
-        environment = "Terraform Web Apache"
-    }
-}
-
-# Create a second virtual machine
-resource "azurerm_linux_virtual_machine" "ultraterra" {
-    name                  = "myVM2"
-    location              = "eastus"
-    resource_group_name   = azurerm_resource_group.myterraformgroup.name
-    network_interface_ids = [azurerm_network_interface.myterraformnic.id]
-    size                  = "Standard_DS1_v2"
-
-    os_disk {
-        name              = "myOsDisk"
-        caching           = "ReadWrite"
-        storage_account_type = "Premium_LRS"
-    }
-
-    source_image_reference {
-        publisher = "Canonical"
-        offer     = "UbuntuServer"
-        sku       = "18.04-LTS"
-        version   = "latest"
-    }
-
-    computer_name  = "myvm"
-    admin_username = "azureuser"
-    disable_password_authentication = true
-
-    admin_ssh_key {
-        username       = "azureuser"
-        public_key     = tls_private_key.example_ssh.public_key_openssh
-    }
-
-    boot_diagnostics {
-        storage_account_uri = azurerm_storage_account.mystorageaccount.primary_blob_endpoint
-    }
-
-    tags = {
-        environment = "Terraform Web Apache"
-    }
-}
 
 # Create a virtual machine
 resource "azurerm_linux_virtual_machine" "myterraVM" {
@@ -246,7 +172,7 @@ resource "azurerm_linux_virtual_machine" "myterraVM" {
     }
 
     tags = {
-        environment = "Terraform Demo"
+        environment = "Terraform Web Apache"
     }
 }
 
